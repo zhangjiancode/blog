99c322761b380cd61f18f3bcadf93a1f37c30935
zhangjian66 <zhangjian66@hikvision.com>
2021-08-09 20:33:29 +0800

    提交前端代码

---
+++ blog-ui/babel.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Description: 
+ * @Author: 张健66
+ * @Date: 2021-08-09 15:47:27
+ * @LastEditTime: 2021-08-09 20:16:01
+ * @LastEditors: 张健66
+ */
+module.exports = {
+  presets: [
+    '@vue/cli-plugin-babel/preset',
+    
+  ]
+}
---
+++ blog-ui/src/views/Home.vue
@@ -0,0 +1,39 @@
+<!--
+ * @Description: 
+ * @Author: 张健66
+ * @Date: 2021-08-09 15:47:27
+ * @LastEditTime: 2021-08-09 16:02:27
+ * @LastEditors: 张健66
+-->
+<template>
+  <div class="home">
+    <div @click="test">123123</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    test () {
+      // 获取远端图片
+      axios({
+        method: 'get',
+        url: 'http://127.0.0.1:3000/api/blog/list',
+        responseType: 'application/json'
+      })
+        .then(function (response) {
+          
+          console.log(response)
+        })
+    }
+  }
+}
+</script>
